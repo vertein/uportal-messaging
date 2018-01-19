794c34322e3d3c9892df81f222cf3ea8f812789f
Timothy Vertein <vertein@wisc.edu>
2018-01-19 09:29:51 -0600

    add servlet initializer

--- src/main/java/edu/Application.java
+++ src/main/java/edu/Application.java
@@ -7,9 +7,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer{
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
